074445528f7a7b00821c6666480e7338d35cdb4d
Yrutan <simon.roy.26@hotmail.com>
2015-06-01 23:52:07 -0400

    200% sur les probabilitées ....

--- Inference/MainForm.Designer.cs
+++ Inference/MainForm.Designer.cs
@@ -39,9 +39,13 @@
             this.LB_Estimation_Ponctuelle = new System.Windows.Forms.Label();
             this.LB_Intervalle = new System.Windows.Forms.Label();
             this.LB_Marge_Erreur = new System.Windows.Forms.Label();
-            this.comboBox1 = new System.Windows.Forms.ComboBox();
-            this.comboBox2 = new System.Windows.Forms.ComboBox();
+            this.CBX_BorneA = new System.Windows.Forms.ComboBox();
+            this.CBX_BorneB = new System.Windows.Forms.ComboBox();
             this.BTN_Executer = new System.Windows.Forms.Button();
+            this.LB_Value_Estimation_Ponctuelle = new System.Windows.Forms.Label();
+            this.LB_Value_Probabilite = new System.Windows.Forms.Label();
+            this.LB_IntervalleConfiance = new System.Windows.Forms.Label();
+            this.LB_Value_Marge_Erreur = new System.Windows.Forms.Label();
             this.SuspendLayout();
             // 
             // RBTN_F1
@@ -61,7 +65,7 @@
             this.RBTN_F2.Location = new System.Drawing.Point(12, 44);
             this.RBTN_F2.Name = "RBTN_F2";
             this.RBTN_F2.Size = new System.Drawing.Size(34, 17);
-            this.RBTN_F2.TabIndex = 1;
+            this.RBTN_F2.TabIndex = 2;
             this.RBTN_F2.TabStop = true;
             this.RBTN_F2.Text = "f2";
             this.RBTN_F2.UseVisualStyleBackColor = true;
@@ -72,7 +76,7 @@
             this.RBTN_F3.Location = new System.Drawing.Point(12, 67);
             this.RBTN_F3.Name = "RBTN_F3";
             this.RBTN_F3.Size = new System.Drawing.Size(34, 17);
-            this.RBTN_F3.TabIndex = 1;
+            this.RBTN_F3.TabIndex = 3;
             this.RBTN_F3.TabStop = true;
             this.RBTN_F3.Text = "f3";
             this.RBTN_F3.UseVisualStyleBackColor = true;
@@ -83,7 +87,7 @@
             this.RBTN_F4.Location = new System.Drawing.Point(12, 90);
             this.RBTN_F4.Name = "RBTN_F4";
             this.RBTN_F4.Size = new System.Drawing.Size(34, 17);
-            this.RBTN_F4.TabIndex = 1;
+            this.RBTN_F4.TabIndex = 4;
             this.RBTN_F4.TabStop = true;
             this.RBTN_F4.Text = "f4";
             this.RBTN_F4.UseVisualStyleBackColor = true;
@@ -94,7 +98,7 @@
             this.RBTN_F5.Location = new System.Drawing.Point(12, 113);
             this.RBTN_F5.Name = "RBTN_F5";
             this.RBTN_F5.Size = new System.Drawing.Size(34, 17);
-            this.RBTN_F5.TabIndex = 1;
+            this.RBTN_F5.TabIndex = 5;
             this.RBTN_F5.TabStop = true;
             this.RBTN_F5.Text = "f5";
             this.RBTN_F5.UseVisualStyleBackColor = true;
@@ -120,7 +124,7 @@
             // LB_Probabilite
             // 
             this.LB_Probabilite.AutoSize = true;
-            this.LB_Probabilite.Location = new System.Drawing.Point(220, 136);
+            this.LB_Probabilite.Location = new System.Drawing.Point(12, 167);
             this.LB_Probabilite.Name = "LB_Probabilite";
             this.LB_Probabilite.Size = new System.Drawing.Size(62, 13);
             this.LB_Probabilite.TabIndex = 2;
@@ -129,7 +133,7 @@
             // LB_Estimation_Ponctuelle
             // 
             this.LB_Estimation_Ponctuelle.AutoSize = true;
-            this.LB_Estimation_Ponctuelle.Location = new System.Drawing.Point(220, 113);
+            this.LB_Estimation_Ponctuelle.Location = new System.Drawing.Point(12, 144);
             this.LB_Estimation_Ponctuelle.Name = "LB_Estimation_Ponctuelle";
             this.LB_Estimation_Ponctuelle.Size = new System.Drawing.Size(113, 13);
             this.LB_Estimation_Ponctuelle.TabIndex = 2;
@@ -138,7 +142,7 @@
             // LB_Intervalle
             // 
             this.LB_Intervalle.AutoSize = true;
-            this.LB_Intervalle.Location = new System.Drawing.Point(220, 159);
+            this.LB_Intervalle.Location = new System.Drawing.Point(12, 190);
             this.LB_Intervalle.Name = "LB_Intervalle";
             this.LB_Intervalle.Size = new System.Drawing.Size(121, 13);
             this.LB_Intervalle.TabIndex = 2;
@@ -147,47 +151,83 @@
             // LB_Marge_Erreur
             // 
             this.LB_Marge_Erreur.AutoSize = true;
-            this.LB_Marge_Erreur.Location = new System.Drawing.Point(220, 181);
+            this.LB_Marge_Erreur.Location = new System.Drawing.Point(12, 212);
             this.LB_Marge_Erreur.Name = "LB_Marge_Erreur";
             this.LB_Marge_Erreur.Size = new System.Drawing.Size(81, 13);
             this.LB_Marge_Erreur.TabIndex = 2;
             this.LB_Marge_Erreur.Text = "Marge d\'erreur :";
             // 
-            // comboBox1
+            // CBX_BorneA
             // 
-            this.comboBox1.FormattingEnabled = true;
-            this.comboBox1.Location = new System.Drawing.Point(279, 23);
-            this.comboBox1.Name = "comboBox1";
-            this.comboBox1.Size = new System.Drawing.Size(54, 21);
-            this.comboBox1.TabIndex = 3;
+            this.CBX_BorneA.FormattingEnabled = true;
+            this.CBX_BorneA.Location = new System.Drawing.Point(279, 23);
+            this.CBX_BorneA.Name = "CBX_BorneA";
+            this.CBX_BorneA.Size = new System.Drawing.Size(54, 21);
+            this.CBX_BorneA.TabIndex = 6;
             // 
-            // comboBox2
+            // CBX_BorneB
             // 
-            this.comboBox2.FormattingEnabled = true;
-            this.comboBox2.Location = new System.Drawing.Point(279, 50);
-            this.comboBox2.Name = "comboBox2";
-            this.comboBox2.Size = new System.Drawing.Size(54, 21);
-            this.comboBox2.TabIndex = 3;
+            this.CBX_BorneB.FormattingEnabled = true;
+            this.CBX_BorneB.Location = new System.Drawing.Point(279, 50);
+            this.CBX_BorneB.Name = "CBX_BorneB";
+            this.CBX_BorneB.Size = new System.Drawing.Size(54, 21);
+            this.CBX_BorneB.TabIndex = 7;
             // 
             // BTN_Executer
             // 
             this.BTN_Executer.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.BTN_Executer.Location = new System.Drawing.Point(223, 224);
+            this.BTN_Executer.Location = new System.Drawing.Point(15, 255);
             this.BTN_Executer.Name = "BTN_Executer";
             this.BTN_Executer.Size = new System.Drawing.Size(78, 29);
-            this.BTN_Executer.TabIndex = 4;
+            this.BTN_Executer.TabIndex = 8;
             this.BTN_Executer.Text = "Exécuter";
             this.BTN_Executer.UseVisualStyleBackColor = true;
             this.BTN_Executer.Click += new System.EventHandler(this.BTN_Executer_Click);
+            // 
+            // LB_Value_Estimation_Ponctuelle
+            // 
+            this.LB_Value_Estimation_Ponctuelle.AutoSize = true;
+            this.LB_Value_Estimation_Ponctuelle.Location = new System.Drawing.Point(131, 144);
+            this.LB_Value_Estimation_Ponctuelle.Name = "LB_Value_Estimation_Ponctuelle";
+            this.LB_Value_Estimation_Ponctuelle.Size = new System.Drawing.Size(0, 13);
+            this.LB_Value_Estimation_Ponctuelle.TabIndex = 5;
+            // 
+            // LB_Value_Probabilite
+            // 
+            this.LB_Value_Probabilite.AutoSize = true;
+            this.LB_Value_Probabilite.Location = new System.Drawing.Point(131, 167);
+            this.LB_Value_Probabilite.Name = "LB_Value_Probabilite";
+            this.LB_Value_Probabilite.Size = new System.Drawing.Size(0, 13);
+            this.LB_Value_Probabilite.TabIndex = 5;
+            // 
+            // LB_IntervalleConfiance
+            // 
+            this.LB_IntervalleConfiance.AutoSize = true;
+            this.LB_IntervalleConfiance.Location = new System.Drawing.Point(131, 190);
+            this.LB_IntervalleConfiance.Name = "LB_IntervalleConfiance";
+            this.LB_IntervalleConfiance.Size = new System.Drawing.Size(0, 13);
+            this.LB_IntervalleConfiance.TabIndex = 5;
+            // 
+            // LB_Value_Marge_Erreur
+            // 
+            this.LB_Value_Marge_Erreur.AutoSize = true;
+            this.LB_Value_Marge_Erreur.Location = new System.Drawing.Point(131, 212);
+            this.LB_Value_Marge_Erreur.Name = "LB_Value_Marge_Erreur";
+            this.LB_Value_Marge_Erreur.Size = new System.Drawing.Size(0, 13);
+            this.LB_Value_Marge_Erreur.TabIndex = 5;
             // 
             // MainForm
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(385, 284);
+            this.ClientSize = new System.Drawing.Size(385, 328);
+            this.Controls.Add(this.LB_Value_Marge_Erreur);
+            this.Controls.Add(this.LB_IntervalleConfiance);
+            this.Controls.Add(this.LB_Value_Probabilite);
+            this.Controls.Add(this.LB_Value_Estimation_Ponctuelle);
             this.Controls.Add(this.BTN_Executer);
-            this.Controls.Add(this.comboBox2);
-            this.Controls.Add(this.comboBox1);
+            this.Controls.Add(this.CBX_BorneB);
+            this.Controls.Add(this.CBX_BorneA);
             this.Controls.Add(this.LB_Estimation_Ponctuelle);
             this.Controls.Add(this.LB_Marge_Erreur);
             this.Controls.Add(this.LB_Intervalle);
@@ -220,9 +260,13 @@
         private System.Windows.Forms.Label LB_Estimation_Ponctuelle;
         private System.Windows.Forms.Label LB_Intervalle;
         private System.Windows.Forms.Label LB_Marge_Erreur;
-        private System.Windows.Forms.ComboBox comboBox1;
-        private System.Windows.Forms.ComboBox comboBox2;
+        private System.Windows.Forms.ComboBox CBX_BorneA;
+        private System.Windows.Forms.ComboBox CBX_BorneB;
         private System.Windows.Forms.Button BTN_Executer;
+        private System.Windows.Forms.Label LB_Value_Estimation_Ponctuelle;
+        private System.Windows.Forms.Label LB_Value_Probabilite;
+        private System.Windows.Forms.Label LB_IntervalleConfiance;
+        private System.Windows.Forms.Label LB_Value_Marge_Erreur;
     }
 }
 
